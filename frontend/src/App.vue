02193f131cf7e52a2aa2667d9171d77c6be26c00
gracechen-0403 <b09902003@ntu.edu.tw>
2024-05-22 10:55:30 +0800

    Fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,31 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+import{reactive, onMounted, toRaw} from 'vue';
+import axios from 'axios';
+
+const test_url = '/api/userdata?uid=123'
+
+export default {
+    setup() {
+        const data = reactive({
+            newsdata:'',
+        })
+        onMounted(() => {
+            console.log(`Try to access Flask backend api: ${test_url}`)
+            axios.get(test_url).then((res)=>{
+              console.log(res.data)
+              data.newsdata = res
+            })
+        });
+    return {data};
+   },
+}
+</script>
+
 <template>
-  <div>
     <header class="nav-bar">
       <div class="wrapper">
-
         <nav>
           <RouterLink to="/" class="nav-link">Home</RouterLink>
           <RouterLink to="/about" class="nav-link">About</RouterLink>
@@ -11,19 +34,6 @@
         </nav>
       </div>
     </header>
-
-    <main class="content">
-      <router-view />
-    </main>
-  </div>
+    <HelloWorld msg="You did it!" />
+  <RouterView />
 </template>
-
-<script>
-import { RouterLink } from 'vue-router'
-
-export default {
-  components: {
-    RouterLink
-  }
-}
-</script>
