6cc8f0e4645b6e4af1888e3e79e872311308dcfa
gracechen-0403 <b09902003@ntu.edu.tw>
2024-05-22 10:49:06 +0800

    Update styles for nav bar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,29 @@
+<template>
+  <div>
+    <header class="nav-bar">
+      <div class="wrapper">
+
+        <nav>
+          <RouterLink to="/" class="nav-link">Home</RouterLink>
+          <RouterLink to="/about" class="nav-link">About</RouterLink>
+          <RouterLink to="/login" class="nav-link">Login</RouterLink>
+          <RouterLink to="/register" class="nav-link">Register</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-
-import{reactive, onMounted, toRaw} from 'vue';
-import axios from 'axios';
-
-const test_url = '/api/userdata?uid=123'
+import { RouterLink } from 'vue-router'
 
 export default {
-    setup() {
-        const data = reactive({
-            newsdata:'',
-        })
-        onMounted(() => {
-            console.log(`Try to access Flask backend api: ${test_url}`)
-            axios.get(test_url).then((res)=>{
-              console.log(res.data)
-              data.newsdata = res
-            })
-        });
-    return {data};
-   },
+  components: {
+    RouterLink
+  }
 }
 </script>
-
-<template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
