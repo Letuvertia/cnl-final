0d03cdb3e4b321ae07b18ef8ad8cd02e23a165c9
hereisIan <ianmustwork@gmail.com>
2024-05-23 01:37:59 +0800

    main new

--- backend/main.py
+++ backend/main.py
@@ -1,53 +1,43 @@
-from flask import Flask, request, jsonify,SQLAlchemy
-from init_db import Message
-#for the test new branch
+from flask import Flask, request, jsonify
+
+from sql import MySQLConnector
+
 api = Flask(__name__)
 
-# Sample user data (replace with your data source)
-user_db = {
-  123: {
-    "name": "John Doe",
+# Sample user data (only for testing)
+user_db = [
+  {
+    "username": "John Doe",
     "email": "john.doe@example.com",
-    "id": 123
   },
-  124: {
-    "name": "Ann Doe",
+  {
+    "username": "Ann Doe",
     "email": "ann.doe@example.com",
-    "id": 124
   }
-}
+]
 
-@api.route("/userdata")
+@api.route("/userdata", methods=['POST'])
 def get_user_data():
   """Returns user data in JSON format"""
   uid = int(request.args.get('uid'))
+
+  if not uid:
+    return jsonify({'error': 'User ID is required'}), 400
+  
   if uid in user_db.keys():
     return jsonify(user_db[uid])
   else:
-    return "User Not Found"
+    return jsonify({'error': 'User not found'}), 404
   
-@api.route("/userdata")
-def get_msg_api():
-  # messages = Message.query.all()
-  # return jsonify([{
-  #   'id':messages.id,
-  #   'content':messages.value,
-  #   'row':messages.row_number,}])
-  return "Hello, World!"
-
-@api.route("/userdata")
-def store_msg_api(msg):
-  messages_data = request.get_json()
-  messages = Message(
-    id = messages_data['id'],
-    content = messages_data['value'],
-  )
-  db = SQLAlchemy(api)
-  db.session.add(messages)
-  db.session.commit()
-  return jsonify({'status':'success store msg'}),201
 
 
 
 if __name__ == "__main__":
-  api.run(host="0.0.0.0", port=5000)
+    # test sql
+    connector = MySQLConnector()
+    connector.add_user(user_db[0])
+    user = connector.get_user(1)
+    print(user)
+
+    # start api server
+    # api.run(host="0.0.0.0", port=5000)
