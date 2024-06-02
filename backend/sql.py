161c9cf4f3dc7240181f43935fc7289e2ce3dcd0
Jun-Wei Liu <1026oscar1@gmail.com>
2024-06-03 01:34:54 +0800

    Fix bugs in sql.py

--- backend/sql.py
+++ backend/sql.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 
 class MySQLConnector:
-    def __init__(self, host="self.db", database="locasync", user="admin", password="admin") -> None:
+    def __init__(self, host="db", database="locasync", user="admin", password="admin") -> None:
         self.connected = False
         self.connection = None
         self.cursor = None
@@ -38,7 +38,7 @@ class MySQLConnector:
                 print(f"Error: {err}")
                 time.sleep(2)
 
-    def _execute_query(self, sql_query, success_log="", param = None) -> None:
+    def _execute_query(self, sql_query, success_log="", param=None) -> None:
         try:
             self.cursor.execute(sql_query, param)
             self.connection.commit()
@@ -82,28 +82,25 @@ class MySQLConnector:
                 msg_id integer
             );
             """
-        self._execute_query(query, "Create table 'message'")
+        self._execute_query(query, "Create table 'likes'")
     
-    def _drop_table(self, table) -> None:
-        query = f"DROP TABLE IF EXISTS {table}"
-        self._execute_query(query, f"Drop table '{table}'")
+    def _truncate_table(self, table) -> None:
+        query = f"TRUNCATE TABLE {table};"
+        self._execute_query(query, f"Empty table '{table}'")
 
-    def _print_table(self, table):
-        query = f"SHOW COLUMNS FROM {table}"
+    def _print_table_schema(self, table):
+        query = f"SHOW COLUMNS FROM {table};"
         self.cursor.execute(query)
         for column in self.cursor.fetchall():
             print(column)
 
     def clear_database(self) -> None:
         """
-        Clear database. Start with empty tables.
+        Empty all tables.
         """
-        self._drop_table("user")
-        self._drop_table("messsage")
-        self._drop_table("likes")
-        self._create_user_table()
-        self._create_message_table()
-        self._create_likes_table()
+        self._truncate_table("user")
+        self._truncate_table("message")
+        self._truncate_table("likes")
 
     def add_user(self, username, password, email,
                  location_latitude=0.0, location_longitude=0.0) -> None:
@@ -113,11 +110,12 @@ class MySQLConnector:
         query = f"""
             INSERT INTO user (username, password, email, location_latitude, location_longitude)
             VALUES (
-            '{username}',
-            '{password}',
-            '{email}',
-            '{location_latitude}',
-            '{location_longitude}');
+                '{username}',
+                '{password}',
+                '{email}',
+                '{location_latitude}',
+                '{location_longitude}'
+            );
         """
         self._execute_query(query, f"Insert user {username}")
     
@@ -142,27 +140,33 @@ class MySQLConnector:
         """
         query = f"""
             INSERT INTO message (msg_userid, msg_likes, msg_content, msg_location_latitude, msg_location_longitude)
-            VALUES ('{msg_userid}',
-                  '0',
-                  '{msg_content}',
-                  '{msg_location_latitude}',
-                  '{msg_location_longitude}');
+            VALUES (
+                '{msg_userid}',
+                '0',
+                '{msg_content}',
+                '{msg_location_latitude}',
+                '{msg_location_longitude}'
+            );
         """
         self._execute_query(query, f"Insert message \"{msg_content}\" by user {msg_userid}")
     
     def get_user_latest_msg(self, userid) -> list:
         """
         Get user's latest message by `userid`.
+        
+        Return: a list of tuple
         """
-        query = f"SELECT * FROM message WHERE userid = {userid} ORDER BY msg_id DESC LIMIT 1;"
+        query = f"SELECT * FROM message WHERE msg_userid = {userid} ORDER BY msg_id DESC LIMIT 1;"
         self._execute_query(query, f"Get the lastest message by user {userid}")
         return self.cursor.fetchall()
     
     def get_user_all_msg(self, userid) -> list:
         """
         Get user's message history by `userid`.
+
+        Return: a list of tuple
         """
-        query = f"SELECT * FROM message WHERE userid = {userid}"
+        query = f"SELECT * FROM message WHERE msg_userid = {userid}"
         self._execute_query(query, f"Get all messages by user {userid}")
         return self.cursor.fetchall()
     
@@ -198,11 +202,11 @@ class MySQLConnector:
         """
         Like the message `msg_id` by the user `userid`.
         """
-        query = f"UPDATE message SET msg_likes = msg_likes + 1 WHERE msg_id = {msg_id};"
-        self._execute_query(query, f"Update message {msg_id} likes")
-
         query = f"INSERT INTO likes (userid, msg_id) VALUES ('{userid}', '{msg_id}');"
         self._execute_query(query, f"Insert record: user {userid} liked message {msg_id}")
+
+        query = f"UPDATE message SET msg_likes = msg_likes + 1 WHERE msg_id = {msg_id};"
+        self._execute_query(query, f"Add 1 like to message {msg_id}")
     
     def is_liked_by_user(self, userid, msg_id) -> bool:
         """
@@ -218,6 +222,13 @@ class Test:
     def __init__(self):
         self.db = MySQLConnector()
         self.db.clear_database()
+
+        print("=====Table 'user'=====")
+        self.db._print_table_schema("user")
+        print("=====Table 'message'=====")
+        self.db._print_table_schema("message")
+        print("=====Table 'message'=====")
+        self.db._print_table_schema("likes")
 
     def run_unit_test(self):
         # test sample data
@@ -247,26 +258,27 @@ class Test:
             print("user exist")
         else:
             raise ValueError("user does not exist")
-        print("=====Table 'user'=====")
-        self.db._print_table("user")
 
         ## msg
         self.db.add_msg(msg_userid=test_msg['msg_userid'],
                         msg_content=test_msg['msg_content'],
                         msg_location_latitude=test_msg['msg_location_latitude'],
-                        msg_location_longitude=test_msg['msg_location_latitude'])
+                        msg_location_longitude=test_msg['msg_location_longitude'])
         print("Latest message: ", self.db.get_user_latest_msg(userid=1))
         print("User 1's message: ", self.db.get_user_all_msg(userid=1))
-        print("=====Table 'message'=====")
-        self.db._print_table("message")
 
         # location
         self.db.update_location(userid=1,
-            latitude=test_msg['msg_location_latitude'], longitude=test_msg['msg_location_latitude'])
+            latitude=test_msg['msg_location_latitude'], longitude=test_msg['msg_location_longitude'])
         print("User 1's location: ", self.db.get_location(userid=1))
         print("Message feed: ", self.db.get_msg_feed_by_location(
-            latitude=test_msg['msg_location_latitude'], longitude=test_msg['msg_location_latitude']))
+            latitude=test_msg['msg_location_latitude'], longitude=test_msg['msg_location_longitude']))
 
         # likes
         self.db.update_likes(userid=1, msg_id=1)
         print("Message 1 liked by user 1: ", self.db.is_liked_by_user(userid=1, msg_id=1))
+        print("User 1's message: ", self.db.get_user_all_msg(userid=1))
+
+if __name__ == "__main__":
+    tester = Test()
+    tester.run_unit_test()
